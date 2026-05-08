2998c4126061c4a8e8cbff4ed77c08598aa2e847
Samiran <palsamiranpal2004@gmail.com>
2026-05-09 00:31:40 +0530

    chore: update frontend dependencies and configure Tailwind CSS
    - Added Tailwind CSS, Autoprefixer, PostCSS, and Typography plugin to devDependencies in package.json.
    - Updated postcss.config.js to use ES module syntax.
    - Modified tailwind.config.js to use ES module syntax and removed Typography plugin from the plugins array.

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
   content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {},
   },
-  plugins: [require('@tailwindcss/typography')],
+  plugins: [],
 };
